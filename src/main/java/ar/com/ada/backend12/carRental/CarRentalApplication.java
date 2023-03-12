92486c4db6af2078310c6dde403db9d7d4ccb8cd
Lucila Kim <lucila.kim.9@gmail.com>
2023-03-12 20:28:26 -0300

    A-4 Add exception handlers.

--- src/main/java/ar/com/ada/backend12/carRental/CarRentalApplication.java
+++ src/main/java/ar/com/ada/backend12/carRental/CarRentalApplication.java
@@ -1,8 +1,10 @@
 package ar.com.ada.backend12.carRental;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+@EnableWebMvc
 @SpringBootApplication
 public class CarRentalApplication {
 
---
+++ src/main/java/ar/com/ada/backend12/carRental/exception/CarRentalExceptionHandler.java
@@ -0,0 +1,33 @@
+package ar.com.ada.backend12.carRental.exception;
+
+import ar.com.ada.backend12.carRental.util.api.ApiReturnable;
+import ar.com.ada.backend12.carRental.util.api.message.ApiMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CarRentalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    protected ResponseEntity<ApiReturnable> handleNotFoundException(NotFoundException exception, WebRequest request) {
+        logger.warn(exception.getMessage(), exception);
+        return new ResponseEntity<>(new ApiMessage(exception.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    protected ResponseEntity<ApiReturnable> handleBadRequestException(BadRequestException exception, WebRequest request) {
+        logger.warn(exception.getMessage(), exception);
+        return new ResponseEntity<>(new ApiMessage(exception.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    protected ResponseEntity<Object> handleAllException(NotFoundException exception, WebRequest request) {
+        logger.error(exception.getMessage(), exception);
+        return new ResponseEntity<>(new ApiMessage("There was an unexpected error."), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
